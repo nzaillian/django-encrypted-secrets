d74436fe1db66463a13db2f4eacbad703af36c2e
Nicholas Zaillian <nzaillian@gmail.com>
2020-06-02 10:21:44 +0700

    Fix for issue wherein load_secrets() fails (in manage.py) if added before init_secrets is run

--- encrypted_secrets/util.py
+++ encrypted_secrets/util.py
@@ -29,7 +29,8 @@ def write_secrets(message, key=secrets_conf.ENCRYPTED_SECRETS_KEY, encrypted_sec
 def read_secrets(encrypted_secrets_file_path=secrets_conf.ENCRYPTED_SECRETS_PATH, key=secrets_conf.ENCRYPTED_SECRETS_KEY):
     key_file_exists = os.path.isfile(encrypted_secrets_file_path)
 
-    if not key_file_exists:
+    # To handle first-run of init_secrets command when key is not yet set:
+    if key is None or not key_file_exists:
         return False
 
     with open(encrypted_secrets_file_path, 'r') as encrypted_secrets_file:
